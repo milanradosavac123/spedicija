b9bc7188f5303026a38fcf656aab3eaa7d2cf53f
MaskedRedstonerProZ <maskedredstonerproz@gmail.com>
2024-03-10 00:38:49 +0100

    made quick chat even more facebook-like

--- src/components/ChatPopupAvatarGroup.tsx
+++ src/components/ChatPopupAvatarGroup.tsx
@@ -3,7 +3,8 @@
 import Conversation from "@/model/Conversation";
 import Avatar from "./chat/Avatar";
 import AvatarGroup from "./chat/AvatarGroup";
-import React from "react";
+import React, { useState } from "react";
+import clsx from "clsx";
 
 interface ChatPopupAvatarGroupProps {
     conversations: Conversation[],
@@ -11,20 +12,31 @@ interface ChatPopupAvatarGroupProps {
 }
 
 export default function ChatPopupAvatarGroup({ conversations, onClick }: ChatPopupAvatarGroupProps) {
+
+    const [isHovering, setIsHovering] = useState(false);
+
     return (
         <div
-            className="flex flex-col-reverse"
+            className={clsx("flex flex-col-reverse", !isHovering && "relative right-10")}
+            onMouseEnter={() => {
+                setIsHovering(true);
+            }}
+            onMouseLeave={() => {
+                setIsHovering(false);
+            }}
         >
             {conversations.map((conversation, i) => (
                 <React.Fragment key={i}>
                     {conversation.isGroup ? (
                         <div
+                            className={clsx(!isHovering && "absolute")}
                             onClick={() => onClick(i)}
                         >
                             <AvatarGroup users={conversation.users} />
                         </div>
                     ) : (
                         <div
+                            className={clsx(!isHovering && "absolute")}
                             onClick={() => onClick(i)}
                         >
                             <Avatar user={conversation.users[3]} />
